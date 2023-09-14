4fe8d264c009323ff3bbebda3d70b73e23536f71
pejman <pejman.hkh@gmail.com>
2023-09-14 21:17:35 +0330

    use parent instead parent()

--- tests/parent.php
+++ tests/parent.php
@@ -3,4 +3,4 @@ include __dir__.'/../vendor/autoload.php';
 
 $p = new \Pejman\DomParser\Parser( '<div class="test">ss<span class="aa">innnerssss</span><span class="aa">innnnn</span></div><div class="test1">eee</div>');
 
-print_r( $p->find(".aa", 0)->parent() );
+var_dump( $p->find(".aa", 0)->parent );
