906fb68ce686c9967634702f5973287e90bfe4dc
pejman <pejman.hkh@gmail.com>
2023-09-14 21:16:58 +0330

    show html for test on github parsing html

--- tests/github.php
+++ tests/github.php
@@ -5,6 +5,7 @@ $p = new \Pejman\DomParser\Parser( file_get_contents('https://github.com/pejman-
 
 print_r( $p->find("#user-repositories-list",0)->find("a",0)->attrs->href );
 
+echo $p->find("#user-repositories-list",0)->html();
 
 $p1 = new \Pejman\DomParser\Parser( file_get_contents(__dir__.'/data/github-bug-in-html.html') );
 
