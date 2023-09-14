49d5614dcd68e7e85bda24dde24cffc07f6a5fba
pejman <pejman.hkh@gmail.com>
2023-09-14 21:17:26 +0330

    use next instead next()

--- tests/next.php
+++ tests/next.php
@@ -3,4 +3,4 @@ include __dir__.'/../vendor/autoload.php';
 
 $p = new \Pejman\DomParser\Parser( '<div class="test">ss<span class="aa">innnerssss</span><span class="aa">innnnn</span></div><div class="test1">eee</div>');
 
-print_r( $p->find(".test", 0)->next() );
+print_r( $p->find(".test", 0)->next );
