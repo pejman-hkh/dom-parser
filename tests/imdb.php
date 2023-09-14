9550943ac530ab4472f70b66e5bbcb97ae4a4ecf
pejman <pejman.hkh@gmail.com>
2023-09-14 22:34:48 +0330

    get attr direct from tag

--- tests/imdb.php
+++ tests/imdb.php
@@ -4,4 +4,4 @@ include __dir__.'/../vendor/autoload.php';
 $p = new \Pejman\DomParser\Parser( file_get_contents(__dir__.'/data/fight-club.txt') );
 
 
-var_dump( $p->find(".ipc-image")[0]->attrs->src );
+var_dump( $p->find(".ipc-image")[0]->src );
