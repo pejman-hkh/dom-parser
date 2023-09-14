abb7dd01650cc681e6f56c9e4b4c4b90b1fdbfc6
pejman <pejman.hkh@gmail.com>
2023-09-14 16:59:40 +0330

    test attr

--- tests/attr.php
+++ tests/attr.php
@@ -1,6 +1,8 @@
 <?php
 include __dir__.'/../vendor/autoload.php';
 
-$p = new \Pejman\DomParser\Parser( '<div class="test">ss<span class="aa">innnerssss</span><span class="aa">innnnn</span></div><div class="test1">eee</div>');
+$p = new \Pejman\DomParser\Parser( '<span class="text-normal" data-menu-button-text>Stars</span><header data-light-theme=light data-dark-theme=dark></header><div id=test></div><div class="test">ss<span class="aa">innnerssss</span><span class="aa">innnnn</span></div><div class="test1">eee</div>');
+
+print_r( $p );
 
 print_r( $p->find(".aa", 0)->attr('aaa','bbb') );
