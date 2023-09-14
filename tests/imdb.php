37101363596d1cac6f2a3b8f32ee430ffc137fa7
pejman <pejman.hkh@gmail.com>
2023-09-14 21:19:12 +0330

    e

--- tests/imdb.php
+++ tests/imdb.php
@@ -3,4 +3,5 @@ include __dir__.'/../vendor/autoload.php';
 
 $p = new \Pejman\DomParser\Parser( file_get_contents(__dir__.'/data/fight-club.txt') );
 
+
 print_r( $p->find(".ipc-image")[0]->attrs->src );
