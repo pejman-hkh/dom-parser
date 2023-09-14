82c8245bbd5305b6c5bf75a9310078eb93d3b735
pejman <pejman.hkh@gmail.com>
2023-09-14 23:08:54 +0330

    some test for query

--- tests/query.php
+++ tests/query.php
@@ -1,5 +1,6 @@
 <?php
 include __dir__.'/../vendor/autoload.php';
 
-$p = new \Pejman\DomParser\Parser( '<div><div class="test">ss</div></div><div class="test1">eee</div>');
-var_dump( $p->find(".test,.test1") );
+$p = new \Pejman\DomParser\Parser( '<div><div class="test"><span></span><span></span></div></div><div class="test1">eee</div>');
+//var_dump( $p->find(".test,.test1") );
+var_dump( $p->find(".test span") );
