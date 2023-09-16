7116f8f60e8d810ef0942f559dd336f04229da11
pejman <pejman.hkh@gmail.com>
2023-09-16 23:51:08 +0330

    define dump function and e function for fast

---
+++ src/Helper.php
@@ -0,0 +1,30 @@
+<?php
+function removeFromObject( &$object ) {
+	$ret = [];
+	if( is_array( $object ) ) {
+		foreach( $object as $o ) {
+			removeFromObject( $o );
+		}
+	} else {
+		if( $object->childrens )
+			removeFromObject( $object->childrens );
+		$object->next = 'Next';
+		$object->prev = 'Prev';
+		$object->parent = 'Parent';
+	}
+	return $object;
+}
+
+if( ! function_exists('dump') ) {
+	function dump( $object ) {
+		$t = unserialize(serialize($object));
+		removeFromObject( $t );
+		var_dump( $t );
+	}
+}
+
+if( ! function_exists('e') ) {
+	function e( $html ) {
+		return new \Pejman\DomParser\Parser( $html );
+	}
+}
