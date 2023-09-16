78d5fb665dddfaa3be30a178a08413227defdebf
pejman <pejman.hkh@gmail.com>
2023-09-16 15:23:04 +0330

    add xml example

---
+++ examples/xml.php
@@ -0,0 +1,5 @@
+<?php
+include __dir__.'/../vendor/autoload.php';
+$p = new \Pejman\DomParser\Parser( file_get_contents(__dir__.'/data/test.xml') );
+
+var_dump( $p->find("last_updated",0)->find("last_updated",0)->html() );
