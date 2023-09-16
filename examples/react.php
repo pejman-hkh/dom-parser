98391e091d705e181bd068fbab89f7b483ecb5ec
pejman <pejman.hkh@gmail.com>
2023-09-16 18:54:16 +0330

    make something like react in php

--- examples/react.php
+++ examples/react.php
@@ -10,13 +10,17 @@ function render( $html ) {
 	$el = e( $html );
 	$ret = '';
 	foreach( $el->document->childrens as $child ) {
-		if( function_exists( $child->tag ) ) {
+
+		if( function_exists( __NAMESPACE__.'\\'.$child->tag ) ) {
 			$fn = $child->tag;
 		
 			$component = call_user_func_array(__NAMESPACE__.'\\'.$fn, [ $child->attrs, render($child->html()) ] );
 			$ret .= $component->document->getHtml();
 		} else {
-			$ret.= $child->makeHtml($child, render( $child->childrens ));			
+			if( $child->tag == 'empty')
+				$ret .= $child->content;
+			else
+				$ret.= $child->makeHtml($child, render( $child->html() ));			
 		}
 
 	}
@@ -24,9 +28,18 @@ function render( $html ) {
 	return $ret;
 }
 
-echo render( '<Header title="test">test</Header>' );
+echo render( '<Header title="test">test <Link to="https://www.google.com/">Google</Link></Header>
+<Footer title="test">test</Footer>' );
 
 
 function Header( $props, $childrens ) {
 	return e('<header><nav><ul><li><a href="/">Home</a></li></ul></nav>'.$childrens.'</header><h1>'.$props->title.'</h1>');
+}
+
+function Link( $props, $childrens ) {
+	return e('<a href="'.$props->to.'">'.$childrens.'</a>');
+}
+
+function Footer( $props, $childrens ) {
+	return e('<footer>'.$childrens.'</footer>');
 }
