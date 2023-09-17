ee4650abe2e9441c14c313d0671c6b3cb8b45310
pejman <pejman.hkh@gmail.com>
2023-09-17 10:46:54 +0330

    i should check this example

--- examples/xml.php
+++ examples/xml.php
@@ -2,4 +2,4 @@
 include __dir__.'/../vendor/autoload.php';
 $p = new \Pejman\DomParser\Parser( file_get_contents(__dir__.'/data/test.xml') );
 
-var_dump( $p->find("last_updated",0)->find("last_updated",0)->html() );
+var_dump( $p->find("last_updated",0)->html() );
