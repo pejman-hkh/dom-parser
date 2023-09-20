ce66c79b47c6306b506f955c19d7196772a770e1
pejman <pejman.hkh@gmail.com>
2023-09-20 16:38:40 +0330

    remove PQuery document from example and use e for fast usage

--- examples/pquery.php
+++ examples/pquery.php
@@ -1,8 +1,6 @@
 <?php
 include __dir__.'/../vendor/autoload.php';
-$p = new \Pejman\DomParser\Parser( '<div id="test"><div class="test"><span>aaa</span>bbb<span>ccc</span></div></div><div class="test1">eee</div>');
-\Pejman\DomParser\PQuery::$document = $p->document;
-
+$p = e( '<div id="test"><div class="test"><span>aaa</span>bbb<span>ccc</span></div></div><div class="test1">eee</div>');
 
 pq("#test span")->each(function( $elm ) {
 	echo pq( $elm )->html()."\n";
