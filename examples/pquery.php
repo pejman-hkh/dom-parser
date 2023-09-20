0be349ad4408c0109e5f28c09819806c8682a8b7
pejman <pejman.hkh@gmail.com>
2023-09-20 16:33:21 +0330

    pq example for addClass and removeClass

--- examples/pquery.php
+++ examples/pquery.php
@@ -9,3 +9,13 @@ pq("#test span")->each(function( $elm ) {
 });
 
 print_r( pq("#test")->next()->attr('class') );
+echo "\n";
+pq("#test")->addClass("added-class");
+echo pq("#test")->outerHtml();
+echo "\n";
+pq("#test")->removeClass("added-class");
+echo pq("#test")->outerHtml();
+echo "\n";
+pq("#test")->addClass("added-class");
+echo pq("#test")->outerHtml();
+echo "\n";
